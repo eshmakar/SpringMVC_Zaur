eb67e586dd1bb3d4ef6e273ba1c2acfe26e1149f
eshmakar <59125020+eshmakar@users.noreply.github.com>
2021-10-06 23:29:57 +0300

    @RequestParam("employeeName") String empName - вместо HttpServletRequest

--- src/main/java/ru/eshmakar/spring/mvc/Controller.java
+++ src/main/java/ru/eshmakar/spring/mvc/Controller.java
@@ -3,6 +3,7 @@ package ru.eshmakar.spring.mvc;
 import org.springframework.http.HttpRequest;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.Arrays;
@@ -26,14 +27,22 @@ public class Controller {
 //        return "show-emp-details-view";
 //    }
 
+//    @RequestMapping("/showDetails")
+//    //все что ввели юзер в поле ввода текста это HttpServletRequest и через request мы получаем данные
+//    //то что получили через request, сохраняем в переменную и добавляем в модел, чтобы потом использовать с модели эти данные
+//    public String showEmpDetails(HttpServletRequest request, Model model) {
+//        String empName = request.getParameter("employeeName");
+//        empName = "Mr. " + empName;
+//        model.addAttribute("nameAttribute", empName);//добавляем в модель по имени nameAttribute - то что ввел юзер
+//        model.addAttribute("description", " - udemy instructor");//можем добавить сколько угодно моделей
+//        return "show-emp-details-view";
+//    }
+
     @RequestMapping("/showDetails")
-    //все что ввели юзер в поле ввода текста это HttpServletRequest и через request мы получаем данные
-    //то что получили через request, сохраняем в переменную и добавляем в модел, чтобы потом использовать с модели эти данные
-    public String showEmpDetails(HttpServletRequest request, Model model) {
-        String empName = request.getParameter("employeeName");
-        empName = "Mr. " + empName;
-        model.addAttribute("nameAttribute", empName);//добавляем в модель по имени nameAttribute - то что ввел юзер
-        model.addAttribute("description", " - udemy instructor");//можем добавить сколько угодно моделей
+    //вместо HttpServletRequest можно использовать @RequestParam, в скобках указывается переменная employeeName, а так же переменная String
+    public String showEmpDetails(@RequestParam("employeeName") String empName, Model model) {
+        empName = "Mr. " + empName + "!";
+        model.addAttribute("nameAttribute", empName);
         return "show-emp-details-view";
     }
 
